059540bec85e820bc39cb40206609cd62d56ba26
NahtigalTim <sirislyyofriend@gmail.com>
2025-11-19 18:28:05 +0100

    Added kill

---
+++ LocationAPI/Controllers/KillController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LocationAPI.Controllers
+{
+	[ApiController]
+	[Route("/kill")]
+	public class KillController : Controller
+	{
+		private readonly IHostApplicationLifetime _lifetime;
+
+		public KillController(IHostApplicationLifetime lifetime)
+		{
+			_lifetime = lifetime;
+		}
+
+		[HttpPut]
+		public IActionResult Put()
+		{
+			Task.Run(() =>
+			{
+				Task.Delay(500).Wait();
+				_lifetime.StopApplication();
+			});
+
+			return Ok("My only regret is not doing this sooner.");
+		}
+	}
+}
